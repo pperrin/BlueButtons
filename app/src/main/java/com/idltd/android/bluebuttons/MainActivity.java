af076a0ce10cdf2405807b4af8c0b4c5a84c69f9
Paul Perrin <pperrin.uk@googlemail.com>
2015-03-11 13:12:08 +0000

    First Hackabout

--- app/src/main/java/com/idltd/android/bluebuttons/MainActivity.java
+++ app/src/main/java/com/idltd/android/bluebuttons/MainActivity.java
@@ -2,6 +2,9 @@ package com.idltd.android.bluebuttons;
 
 import android.app.*;
 import android.os.*;
+import android.content.IntentFilter;
+import android.content.Intent;
+import android.widget.*;
 
 public class MainActivity extends Activity 
 {
@@ -10,5 +13,15 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
+		Toast.makeText(getApplication(),"here",Toast.LENGTH_SHORT);
+		android.util.Log.d("a","b");
+		
+    IntentFilter filter = new IntentFilter(Intent.ACTION_MEDIA_BUTTON);
+	filter.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY^100);
+    MediaButtonIntentReceiver r = new MediaButtonIntentReceiver();
+    registerReceiver(r, filter); 
+	Toast.makeText(getApplication(),"there",Toast.LENGTH_SHORT);
+		android.util.Log.d("c",Integer.toString(IntentFilter.SYSTEM_HIGH_PRIORITY));
+
+	}
 }
---
+++ app/src/main/java/com/idltd/android/bluebuttons/MediaButtonIntentReceiver.java
@@ -0,0 +1,33 @@
+package com.idltd.android.bluebuttons;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.view.KeyEvent;
+
+public class MediaButtonIntentReceiver extends BroadcastReceiver {
+
+    public MediaButtonIntentReceiver() {
+        super();
+		android.util.Log.d("broadcast media event", "create");
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+//		android.widget.Toast.makeText(context.getApplicationContext(),"got one!",android.widget.Toast.LENGTH_LONG);
+		android.util.Log.d("broadcast media event", "blah");
+        String intentAction = intent.getAction();
+        if (!Intent.ACTION_MEDIA_BUTTON.equals(intentAction)) {
+            return;
+        }
+        KeyEvent event = (KeyEvent)intent.getParcelableExtra(Intent.EXTRA_KEY_EVENT);
+        if (event == null) {
+            return;
+        }
+        int action = event.getAction();
+        if (action == KeyEvent.ACTION_DOWN) {
+			// do something
+        }
+        abortBroadcast();
+    }
+}
